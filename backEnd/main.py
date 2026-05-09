8dc27d8cefc12f52474c4939bfc0adf705741b92
selva7thsens-eng <selva7thsens@gmail.com>
2026-05-09 22:20:25 +0530

    fix cors

--- backEnd/main.py
+++ backEnd/main.py
@@ -14,7 +14,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+        allow_origins=[
+        "https://crtapp-gamma.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
