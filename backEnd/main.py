0cc4dad55990f2fa64af557e9ca965e4ec72a67b
selva7thsens-eng <selva7thsens@gmail.com>
2026-05-12 15:07:53 +0530

    allow Backend

--- backEnd/main.py
+++ backEnd/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://crtapp-gamma.vercel.app"
+        "https://crtapp-backend-q4xk.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
