24181a1d060a03343b43d9e0f59e9fd68cdd0cdf
selva7thsens-eng <selva7thsens@gmail.com>
2026-05-12 15:13:33 +0530

    Backend method upadte

--- backEnd/main.py
+++ backEnd/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://crtapp-backend-q4xk.onrender.com"
+         "https://crtapp-git-main-selva-s-projects1.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
